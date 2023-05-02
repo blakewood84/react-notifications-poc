fe599a58fd262d952f7b610dba2a154f442325c1
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-05-02 09:57:05 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { StreamClient } from 'getstream';
+import { StreamApp } from 'react-activity-feed';
+import ScrollToTop from './components/ScrollToTop';
+import StartPage from './pages/StartPage';
+import { useEffect, useState } from 'react';
+import { getFromStorage } from './utils/storage';
+import users from './users';
+import HomePage from './pages/HomePage';
 
-function App() {
+const APP_ID = '1241926';
+const API_KEY = 'x2u4y9padhyg';
+
+export default function App() {
+  const userId = getFromStorage('user');
+
+  const user = users.find((u) => u.id === userId) || user[0];
+
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    console.log('user: ', user);
+    async function init() {
+      const client = new StreamClient(API_KEY, user.token, APP_ID);
+      await client.user(user.id).getOrCreate({ ...user, token: '' });
+
+      setClient(client);
+    }
+
+    init();
+  }, []);
+
+  if (!client) return <></>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StreamApp token={user.token} appId={APP_ID} apiKey={API_KEY}>
+      <Router>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </StreamApp>
   );
 }
-
-export default App;
---
+++ src/users.js
@@ -0,0 +1,44 @@
+const users = [
+  {
+    id: 'iamdillion',
+    name: 'Dillion',
+    image: 'https://dillionmegida.com/img/deee.jpg',
+    bio: 'Just here, doing my thing. Developer advocate at @getstream_io',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaWFtZGlsbGlvbiJ9.0xb34N5-qb9Xe4wZ-kQqYBJUyqqo7Kk_f9ov7dHfl5k',
+  },
+  {
+    id: 'getstream_io',
+    name: 'Stream',
+    image: 'https://avatars.githubusercontent.com/u/8597527?s=200&v=4',
+    bio: 'Deploy activity feeds and chat at scale with Stream – an API driven platform powering over a billion end users. Get started at http://getstream.io.',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZ2V0c3RyZWFtX2lvIn0.4o2MGReGn3xriR7_SfzvJ0RM-vBR22GAwImY78wXyVs',
+  },
+  {
+    id: 'jake',
+    name: 'Jake',
+    image: 'https://picsum.photos/300/300',
+    bio: 'Just Jake, nothing much',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFrZSJ9.A1xODLbG_JuAS8zpNIvyHCKDn2t8TSeqn5r_IjmAHaU',
+  },
+  {
+    id: 'joe',
+    name: 'Joe',
+    image: 'https://picsum.photos/200/200',
+    bio: 'How are you?',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9lIn0.RyWwAdYo_yXxQz8thXRrJZWtsHB0zu9vuMICWYhv6Ao',
+  },
+  {
+    id: 'mike',
+    name: 'Mike',
+    image: 'https://picsum.photos/400/400',
+    bio: 'I am mike here. I do things on #react and #javascript',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.fV2DIaZc49EIF35RUlL4CDreTqoE2Gt8iEig7AKNt5c',
+  },
+];
+
+export default users;
